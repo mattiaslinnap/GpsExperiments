ef66387143a63c87247c54ecc71038c2854afee5
Mattias Linnap <mattias@linnap.com>
2010-09-02 22:19:38 +0100

    Initial Android project.

---
+++ src/uk/ac/cam/cl/gpsexperiments/GpsExperimentsUI.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.cl.gpsexperiments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GpsExperimentsUI extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
